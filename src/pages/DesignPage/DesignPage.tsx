067f294c59edee5fdba85c29824841367ce129fa
Jawad-git <mohammed.aljawadd@gmail.com>
2025-03-16 23:55:37 +0200

    make the portfolio persistent

--- src/pages/DesignPage/DesignPage.tsx
+++ src/pages/DesignPage/DesignPage.tsx
@@ -4,10 +4,7 @@ import styles from "./DesignPage.module.css";
 import Table from "../../Components/Table/Table";
 import RatioList from "../../Components/RatioList/RatioList";
 import { CompanyKeyMetrics } from "../../company";
-import {
-  TestDataCompany,
-  testIncomeStatementData,
-} from "../../Components/Table/testData";
+import { TestDataCompany, testIncomeStatementData } from "../../Components/Table/testData";
 interface Props {}
 const tableConfig = [
   {
@@ -21,15 +18,13 @@ const DesignPage = (props: Props) => {
   return (
     <>
       <h1>
-        Design guide - this is the design guide for Fin Shark. These are
-        reusable components of the app with brief instructions on how to use
-        them.
+        Design guide - this is the design guide for Fin Shark. These are reusable components of the app with brief
+        instructions on how to use them.
       </h1>
       <RatioList data={testIncomeStatementData} config={tableConfig} />
       <Table config={tableConfig} data={testIncomeStatementData} />
       <h3>
-        Table - table takes in a configuration object and company data as
-        params. Use the config to style your table.
+        Table - table takes in a configuration object and company data as params. Use the config to style your table.
       </h3>
     </>
   );
